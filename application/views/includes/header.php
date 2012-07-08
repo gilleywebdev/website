ed5af999448707627dad2a0b9f5cde5a705527b6
Chris Gilley <chris@gilleywebdev.com>
2012-07-08 16:42:32 -0400

    Favicon & Facebook thumb

--- application/views/includes/header.php
+++ application/views/includes/header.php
@@ -1,3 +1,16 @@
+<!-- SEO -->
 <title><?php echo $title; ?></title>
 <meta name="description" content="<?php echo $description; ?>">
+
+<!-- Styles -->
 <link rel="stylesheet" href="/css/styles.css" type="text/css">
+
+<!-- Vanity -->
+<link rel="shortcut icon" href="favicon.ico">
+<?php
+	// Facebook thumnbail on production
+	if(Kohana::$environment !== Kohana::PRODUCTION && $_SERVER['SERVER_NAME'])
+	{
+		echo '<meta property="og:image" content="'.$_SERVER['SERVER_NAME'].'/graphics/fb-thumb.jpg">';
+	}
+?>

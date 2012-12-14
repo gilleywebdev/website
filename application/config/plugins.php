281f253773a81dab8c6f3286a8ece03f0c366d8e
Chris Gilley <chris@gilleywebdev.com>
2012-12-14 16:29:32 -0500

    Plugins revamp tentative final draft

--- application/config/plugins.php
+++ application/config/plugins.php
@@ -1,9 +1,5 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-return array
-(
-	'enabled' => array
-		(
-			'default' => array('jscrollpane'),
-		)
+return array(
+	'default' => array('jscrollpane')
 );

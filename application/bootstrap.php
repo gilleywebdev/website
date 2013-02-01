52ca6610f9852235a200a9b60dd199f8c5393b3f
Chris Gilley <chris@gilleywebdev.com>
2013-01-31 21:46:38 -0500

    Added DOMAIN stuff back into bootstrap

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -139,6 +139,8 @@ if (PHP_SAPI !== 'cli')
 	// Attach database reader/writer for config
 	Kohana::$config->load('database');
 	Kohana::$config->attach(new Config_Database);
+	
+	define('DOMAIN', $_SERVER['SERVER_NAME']);
 }
 /**
  * Set the routes. Each route must have a minimum of a name, a URI and a set of
